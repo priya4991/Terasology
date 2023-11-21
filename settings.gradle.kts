c9bd168f299073dda0e199e4820c97677d1f0140
soloturn <soloturn@gmail.com>
2023-11-21 04:14:32 +0100

    add refreshversions plugin, to centralize version spec.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.3"
+}
+
 rootProject.name = "Terasology"
 
 includeBuild("build-logic")
